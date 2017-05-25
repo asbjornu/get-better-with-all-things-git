d230a7d7806b35187661406236dd6fc252a25e3e
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2017-05-26 00:41:49 +0200

    Removed gunk and painted the circles gradient blue

--- docs/js/graph.js
+++ docs/js/graph.js
@@ -1,7 +1,7 @@
 var graph = d3.select(".graph")
               .append("svg")
-              .attr("width", 200)
-              .attr("height", 200);
+              .attr("width", window.innerWidth)
+              .attr("height", window.innerHeight);
 
 Reveal.initialize();
 Reveal.addEventListener('slidechanged', drawGraph);
@@ -12,49 +12,71 @@ function drawGraph(event) {
     var id = Reveal.getCurrentSlide().id;
     var step = Reveal.getIndices().f;
 
-    console.log(id, step);
-
     if (id != 'dag1') {
         return;
     }
 
-    switch (step) {
-        case 0:
-            var nodes = [
-                {
-                    x: 10,
-                    y: 50
-                },
-                {
-                    x: 70,
-                    y: 10
-                },
-                {
-                    x: 140,
-                    y: 50
-                }
-            ]
+    // Define the gradient
+    var gradient = graph.append("svg:defs")
+        .append("svg:linearGradient")
+        .attr("id", "gradient")
+        .attr("x1", 0)
+        .attr("y1", 0)
+        .attr("x2", 0)
+        .attr("y2", 1)
+        .attr("spreadMethod", "pad");
 
-            graph.selectAll("circle.nodes")
-                .data(nodes)
-                .enter()
-                .append("svg:circle")
-                .attr("cx", function(d) {
-                    return d.x;
-                })
-                .attr("cy", function(d) {
-                    return d.y;
-                })
-                .attr("r", "10px")
-                .attr("fill", "white");
+    // Define the gradient colors
+    gradient.append("svg:stop")
+        .attr("offset", "0%")
+        .attr("stop-color", "#e4f5fc")
+        .attr("stop-opacity", 1);
+
+    gradient.append("svg:stop")
+        .attr("offset", "100%")
+        .attr("stop-color", "#2ab0ed")
+        .attr("stop-opacity", 1);
+
+    switch (step) {
+        case -1:
+        var nodes = [
+            {
+                x: 100,
+                y: 20
+            },
+            {
+                x: 300,
+                y: 20
+            },
+            {
+                x: 500,
+                y: 20
+            }
+        ]
+
+        graph.selectAll("circle.nodes")
+            .data(nodes)
+            .enter()
+            .append("svg:circle")
+            .attr("cx", function(d) {
+                return d.x;
+            })
+            .attr("cy", function(d) {
+                return d.y;
+            })
+            .attr("r", "20px")
+            .attr("fill", "url(#gradient)");
+
+
+            break;
+
+        case 0:
             break;
 
         case 1:
-            // move to vis state 1
             break;
 
         case 2:
-            // move to vis state 2
             break;
     }
 }
